b9fd31bf65369cb5c24378351e4cc8666eea2364
Victor GUEGAN <victorguegan@orange.fr>
2022-03-10 16:10:29 +0100

    swap to vuex

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import 'virtual:windi.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import i18n from './modules/i18n';
-import router from './modules/router';
+import router from './router/';
 
 createApp(App).use(i18n).use(router).mount('#app');
