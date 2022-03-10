e83c8abc7e07eacd8b31128fa5ae259d646511a1
Victor GUEGAN <victorguegan@orange.fr>
2022-03-10 18:15:58 +0100

    added basic hero and lang/dark mode support

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <router-view></router-view>
+  <div class="bg-black text-white dark:(bg-amber-200 text-black) w-full min-h-screen">
+    <Hero></Hero>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script setup lang="ts">
