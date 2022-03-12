f5ba33c01804f37ff755de5754b3be9f3948d5c3
Victor GUEGAN <victorguegan@orange.fr>
2022-03-12 11:13:10 +0100

    design and filter overhaul

--- src/views/CharacterList.vue
+++ src/views/CharacterList.vue
@@ -1,66 +1,115 @@
 <template>
   <div class="mx-auto">
-    <form class="m-20" @submit.prevent="filterData">
-      <input v-model="nameFilter" type="text" class="text-black" />
-      <button type="submit">{{ $t('search') }}</button>
-
-      <fieldset>
-        <input id="radio_none" v-model="statusFilter" type="radio" name="status" v-bind:value="''" />
-        <label for="none">{{ $t('none') }}</label>
-        <input id="radio_alive" v-model="statusFilter" type="radio" name="status" value="alive" />
-        <label for="alive">{{ $t('alive') }}</label>
-        <input id="radio_dead" v-model="statusFilter" type="radio" name="status" value="dead" />
-        <label for="dead">{{ $t('dead') }}</label>
-        <input id="radio_unknown" v-model="statusFilter" type="radio" name="status" value="unknown" />
-        <label for="unknown">{{ $t('unknown') }}</label>
-      </fieldset>
-    </form>
+    <div class="mx-auto w-min">
+      <form class="flex items-center" @submit.prevent="filterData">
+        <div class="flex items-center space-x-2">
+          <div class="flex items-center overflow-hidden rounded">
+            <input
+              v-model="nameFilter"
+              class="relative flex-auto block w-full h-10 min-w-0 px-3 m-0 text-base font-normal text-gray-700 transition ease-in-out bg-white border border-gray-300 border-solid form-control bg-clip-padding focus:text-gray-700 focus:bg-white focus:border-green-600 focus:outline-none"
+              type="text"
+            />
+            <button
+              class="flex items-center inline-block h-10 px-6 text-xs font-medium leading-tight text-white uppercase transition duration-150 ease-in-out bg-green-600 shadow-md btn hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
+              type="submit"
+            >
+              {{ $t('search') }}
+            </button>
+          </div>
+          <p
+            v-if="(router.currentRoute.value.query.name ?? '') !== nameFilter"
+            class="text-sm text-green-600"
+          >
+            {{ $t('refresh-alert') }}
+          </p>
+        </div>
+        <div class="flex-grow" />
+        <fieldset class="flex space-x-4">
+          <div class="flex items-center space-x-0.5">
+            <input
+              id="radio_alive"
+              type="checkbox"
+              class="w-5 h-5 accent-green-600 form-checkbox"
+              v-model="statusCheckBoxes.alive"
+              @change="check('alive')"
+            />
+            <label for="alive">{{ $t('alive') }}</label>
+          </div>
+          <div class="flex items-center space-x-0.5">
+            <input
+              id="radio_dead"
+              type="checkbox"
+              class="w-5 h-5 accent-green-600 form-checkbox"
+              v-model="statusCheckBoxes.dead"
+              @change="check('dead')"
+            />
+            <label for="dead">{{ $t('dead') }}</label>
+          </div>
+          <div class="flex items-center space-x-0.5">
+            <input
+              id="radio_unknown"
+              type="checkbox"
+              class="w-5 h-5 accent-green-600 form-checkbox"
+              v-model="statusCheckBoxes.unknown"
+              @change="check('unknown')"
+            />
+            <label for="unknown">{{ $t('unknown') }}</label>
+          </div>
+        </fieldset>
+      </form>
+      <div
+        v-if="characterList.length > 0"
+        class="grid grid-cols-2 gap-8 mx-auto mt-4 w-max place-items-stretch sm:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"
+      >
+        <CharacterCard
+          :id="c.id"
+          :name="c.name"
+          :status="c.status.toLowerCase()"
+          :imageURL="c.image"
+          v-for="c in characterList"
+          :key="c.id"
+        ></CharacterCard>
+      </div>
+      <div class="mx-auto min-w-75vw" v-else>{{ $t('no-result') }}</div>
+    </div>
     <div
       v-if="store.state.characterStore.info.pages > 1"
-      class="flex items-center text-2xl space-x-2"
+      class="flex items-center justify-center my-8 space-x-2 text-2xl"
     >
       <div class="flex items-center space-x-2">
-        <button class="bg-white bg-opacity-10 rounded-full" @click="navigate(-1)">
+        <button class="bg-white rounded-full bg-opacity-10" @click="navigate(-1)">
           <icon-mdi-chevron-left />
         </button>
       </div>
       <span>{{ currentPage }}</span>
       <div class="flex items-center space-x-2">
         <span>&nbsp;/ {{ store.state.characterStore.info.pages }}</span>
-        <button class="bg-white bg-opacity-10 rounded-full" @click="navigate(1)">
+        <button class="bg-white rounded-full bg-opacity-10" @click="navigate(1)">
           <icon-mdi-chevron-right />
         </button>
       </div>
     </div>
-    <div
-      v-if="characterList.length > 0"
-      class="grid grid-cols-2 gap-8 mx-auto w-max place-items-stretch sm:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"
-    >
-      <CharacterCard
-        :id="c.id"
-        :name="c.name"
-        :status="c.status.toLowerCase()"
-        :imageURL="c.image"
-        v-for="c in characterList"
-        :key="c.id"
-      ></CharacterCard>
-    </div>
-    <div v-else>{{ $t('no-result') }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed, ref, watch } from 'vue';
-import { LocationQueryValue, useRouter } from 'vue-router';
+import { LocationQueryRaw, useRouter } from 'vue-router';
 import { useStore } from 'vuex';
 import { APIParams } from '../types/interfaces';
 
 const store = useStore();
 const router = useRouter();
 
-const computePageNumber = () => {
+const statusCheckBoxes = ref({
+  dead: ref(false),
+  alive: ref(false),
+  unknown: ref(false),
+});
+
+const computePageNumber = (): number | undefined => {
   const pageNb = Number(router.currentRoute.value.query.page);
-  if (!pageNb) return null;
+  if (!pageNb) return undefined;
   if (isNaN(pageNb)) {
     goToPage(1);
     return 1;
@@ -68,17 +117,35 @@ const computePageNumber = () => {
   return pageNb;
 };
 
-const computePageStatusName = (queryParam: LocationQueryValue | LocationQueryValue[]): string | undefined => {
-  console.log(queryParam)
-  if (!queryParam) {
-    return undefined;
-  }
-  return String(queryParam);
-}
+const computePageName = (): string | undefined => {
+  const charName = router.currentRoute.value.query.name;
+  if (!charName) return undefined;
+  return String(charName);
+};
 
-const statusFilter = ref(computePageStatusName(router.currentRoute.value.query.status) ?? '');
-const nameFilter = ref(computePageStatusName(router.currentRoute.value.query.name) ?? '');
+const computePageStatus = (): string | undefined => {
+  const charStatus = router.currentRoute.value.query.status;
+  if (!charStatus) return undefined;
+  const stringParam = String(charStatus) as 'dead' | 'alive' | 'unknown';
+  statusCheckBoxes.value[stringParam] = true;
+  return stringParam;
+};
+
+const statusFilter = ref(computePageStatus() ?? '');
+const nameFilter = ref(computePageName() ?? '');
 const currentPage = ref(computePageNumber() ?? 1);
+
+const check = (status: 'dead' | 'alive' | 'unknown') => {
+  let oneChecked = false;
+  Object.keys(statusCheckBoxes.value).forEach((key) => {
+    const k = key as 'dead' | 'alive' | 'unknown';
+    const v = !statusCheckBoxes.value[k];
+    statusCheckBoxes.value[k] = v ? !v : key === status;
+    oneChecked = statusCheckBoxes.value[k] || oneChecked;
+    if (statusCheckBoxes.value[k]) statusFilter.value = key;
+  });
+  if (!oneChecked) statusFilter.value = '';
+};
 
 const characterList = computed(() => store.state.characterStore.characters);
 
@@ -88,7 +155,6 @@ const loadData = () => {
     name: nameFilter.value,
     page: currentPage.value,
   };
-  console.log(params);
   store.dispatch('fetchCharacters', params);
 };
 loadData();
@@ -118,7 +184,7 @@ const goToPage = (pageNb: number) => {
   if (currentPage.value) params.page = currentPage.value;
   router.push({
     path: '/characters',
-    query: Object.assign({ ...router.currentRoute.value.query }, params),
+    query: params as LocationQueryRaw,
   });
 };
 </script>
